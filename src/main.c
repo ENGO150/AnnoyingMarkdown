2bace4baa92049469b42425c9c373c8d74e5e891
ENGO150 <v.smejkal06@gmail.com>
2022-04-15 15:47:51 +0200

    exit fix

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+#define MISSING_ARG 1
+
 int
 main(int argc, char **argv)
 {
     if (argc == 1)
     {
         printf("Sorry, you must enter text as arg... \"./AnnoyingMarkdown TEXT_HERE\"");
+        exit(MISSING_ARG);
     }
 
     return 0;
