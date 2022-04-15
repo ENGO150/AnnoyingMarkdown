5656d4e5a799123813c2de62a8b8b989a2c9c11a
ENGO150 <v.smejkal06@gmail.com>
2022-04-15 16:57:02 +0200

    created first working version

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MISSING_ARG 1
 
@@ -11,5 +13,38 @@ main(int argc, char **argv)
         exit(MISSING_ARG);
     }
 
+    //VARIABLES
+    int outputSize = strlen(argv[1]) * 5; //SIZE CALCULATION ≈ 5x
+    int textNumberBuffer = 2; //THIS IS FOR COUNTING POSITIONS OF '||'
+    int indexNumberBuffer = 0; //THIS IS FOR GETTING TEXT FROM ARG
+    char *output = malloc(outputSize);
+
+    //LOAD output
+    for (int i = 0; i < outputSize; i++)
+    {
+        loopStart:
+
+        if (i < textNumberBuffer) //SPACE FOR '|'
+        {
+            output[i] = '|';
+            continue;
+        }
+
+        if (i == textNumberBuffer) //SPACE FOR TEXT
+        {
+            output[i] = argv[1][indexNumberBuffer];
+            indexNumberBuffer++;
+            continue;
+        }
+
+        if (i > textNumberBuffer) //OUT OF BUFFER
+        {
+            textNumberBuffer += 5;
+            goto loopStart;
+        }
+    }
+
+    printf("%s\n", output); //PRINT OUT OUTPUT
+    
     return 0;
 }
